db4bd5227faa2f3756c3b728444af647e6b92ec2
Serigne Amsatou Seye <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-11-03 16:46:13 +0100

    Ajout de l'option -model dans l'exécution de Glucose et redirection du resultat dans un fichier result

--- hac2sat.sh
+++ hac2sat.sh
@@ -33,7 +33,7 @@ while [[ -z $reponse || $reponse == 'O' ]]; do
     #./hac2sat $k | tail -n 1 | grep -q 'E' && echo "Y a un problème" # Exécuter le programme pour générer le fichier de format DIMACS
     TEST_OUT=$(./hac2sat $k 2>&1)
     echo $TEST_OUT
-    echo $TEST_OUT | grep -v 'Usage' && ./glucose-syrup/simp/glucose formule.cnf # Lancer le solveur s'il n'y a pas erreur d'entrée
+    echo $TEST_OUT | grep -v 'Usage' && ./glucose-syrup/simp/glucose -model formule.cnf > result # Lancer le solveur s'il n'y a pas erreur d'entrée
 
 
     reponse=''
