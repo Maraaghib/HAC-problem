75de1e422344e517eb99b3d21f5812d486c9efe4
Serigne Amsatou SEYE <hamzatou10@hotmail.fr>
2017-11-03 04:48:37 +0100

    Ajout du solveur Glucose 4.0

---
+++ glucose-syrup/simp/depend.mk
@@ -0,0 +1,39 @@
+/home/hamza/projets/HAC-problem/glucose-syrup/simp/SimpSolver.o /home/hamza/projets/HAC-problem/glucose-syrup/simp/SimpSolver.or /home/hamza/projets/HAC-problem/glucose-syrup/simp/SimpSolver.od /home/hamza/projets/HAC-problem/glucose-syrup/simp/SimpSolver.op: \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/SimpSolver.cc \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Sort.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Vec.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/IntTypes.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/XAlloc.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../simp/SimpSolver.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Queue.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../core/Solver.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Heap.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Alg.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../utils/Options.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../utils/ParseUtils.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../core/SolverTypes.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Map.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Alloc.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../core/BoundedQueue.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../core/Constants.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Clone.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../utils/System.h
+/home/hamza/projets/HAC-problem/glucose-syrup/simp/Main.o /home/hamza/projets/HAC-problem/glucose-syrup/simp/Main.or /home/hamza/projets/HAC-problem/glucose-syrup/simp/Main.od /home/hamza/projets/HAC-problem/glucose-syrup/simp/Main.op: /home/hamza/projets/HAC-problem/glucose-syrup/simp/Main.cc \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../utils/System.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/IntTypes.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../utils/ParseUtils.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../utils/Options.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Vec.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/XAlloc.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../core/Dimacs.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../core/SolverTypes.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Alg.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Map.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Alloc.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../simp/SimpSolver.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Queue.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../core/Solver.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Heap.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../core/BoundedQueue.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../core/Constants.h \
+ /home/hamza/projets/HAC-problem/glucose-syrup/simp/../mtl/Clone.h
